00b99a95e20a92da9c660f1712b73be5d208cd5f
Shai Sussman <shai.su@kaplanopensource.co.il>
2022-02-08 07:34:22 +0200

    import flask

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import geopandas as gpd
 import numpy as np
-import Flask
+from flask import Flask
 import datetime
 import pandas as pd
 import json
